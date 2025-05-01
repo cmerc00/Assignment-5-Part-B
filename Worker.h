ff49fe53ed77e4843220527329f9c4c8c36dadcd
cmerc00 <cmercadel001@gmail.com>
2025-04-30 23:42:26 -0700

    Create Worker.h

---
+++ Worker.h
@@ -0,0 +1,36 @@
+//
+//  Doctor.hpp
+//  Assignment #5 Part B
+//
+//  Created by Cherelle Mercadel on 4/30/25.
+//
+
+#ifndef Doctor_h
+#define Doctor_h
+
+#include "Worker.h"
+
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+class Doctor : public Worker
+{
+private:
+    string DoctName;
+    double DoctSalary;
+    
+public:
+    Doctor();
+    Doctor(string n, double s);
+    
+    void setName(string n);
+    void setSalary(double s);
+    
+    double getSalary();
+    string getName();
+    
+    double salaryPerWeek();
+};
+
+#endif /* Doctor_hpp */
