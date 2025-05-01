84fb525b960fd661c6251cad665fb25d46876fd6
cmerc00 <cmercadel001@gmail.com>
2025-04-30 23:50:27 -0700

    Create Sales Person.c

---
+++ Sales Person.c
@@ -0,0 +1,42 @@
+//
+//  Sales Person.cpp
+//  Assignment #5 Part B
+//
+//  Created by Cherelle Mercadel on 4/30/25.
+//
+
+#include "Sales Person.h"
+Sales::Sales() : Worker()
+{
+    
+}
+Sales::Sales(string n, double s) : Worker( n, s)
+{
+    
+}
+void Sales::setName(string n)
+{
+    SalesName = n;
+}
+void Sales::setSalary(double s)
+{
+    SalesSalary = s;
+}
+
+double Sales::getSalary()
+{
+    return SalesSalary;
+}
+string Sales::getName()
+{
+    return SalesName;
+}
+
+double Sales::salaryPerWeek()
+{
+    int yr = 52;
+    double sal;
+    
+    sal = SalesSalary / yr;
+    return sal;
+}
